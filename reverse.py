bd5283f9d5b4930ea30811101aab58836d754621
ebtrader <jsiddique@gmail.com>
2022-06-16 06:51:33 -0400

    Initial Commit

--- reverse.py
+++ reverse.py
@@ -1,6 +1,8 @@
 # https://www.datasciencemadesimple.com/reverse-the-rows-of-the-dataframe-in-pandas-python-2/
 
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 
 df1 = {
@@ -12,3 +14,31 @@ print(df1)
 
 df2 = df1.iloc[::-1]
 print(df2)
+
+# https://www.geeksforgeeks.org/how-to-combine-two-dataframe-in-python-pandas/
+
+frames = [df1, df2]
+
+result = pd.concat(frames)
+
+print(result)
+
+# https://www.machinelearningplus.com/pandas/pandas-reset-index/
+
+result = result.reset_index(drop=True)
+
+print(result)
+
+fig = px.scatter(result, x=result.index, y="Score")
+
+fig.write_html( 'output_file_name.html',
+                   auto_open=True )
+
+# fig1 = go.Figure(data=[go.Candlestick(x=df['Date'],
+#                                       open=df['Open'],
+#                                       high=df['High'],
+#                                       low=df['Low'],
+#                                       close=df['Close'], showlegend=False)])
+#
+# fig1.write_html( 'output_file_name.html',
+#                    auto_open=True )
