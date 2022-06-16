de5500ac20d7307f82ecafcd38000bf432bed7cd
ebtrader <jsiddique@gmail.com>
2022-06-16 06:57:02 -0400

    Initial Commit

--- reverse.py
+++ reverse.py
@@ -5,11 +5,14 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 
-df1 = {
-    'State':['Arizona AZ','Georgia GG','Newyork NY','Indiana IN','Florida FL'],
-   'Score':[62,47,55,74,31]}
+# df1 = {
+#     'State':['Arizona AZ','Georgia GG','Newyork NY','Indiana IN','Florida FL'],
+#    'Score':[62,47,55,74,31]}
 
-df1 = pd.DataFrame(df1, columns=['State', 'Score'])
+df1 = {'Score':[55,59,60,58,56,62,59,55,63,60]}
+
+
+df1 = pd.DataFrame(df1, columns=['Score'])
 print(df1)
 
 df2 = df1.iloc[::-1]
@@ -29,7 +32,11 @@ result = result.reset_index(drop=True)
 
 print(result)
 
-fig = px.scatter(result, x=result.index, y="Score")
+# fig = px.scatter(result, x=result.index, y="Score")
+
+fig = px.line(result, x=result.index, y="Score")
+
+fig.add_vline(x=9.5, line_width=3, line_dash="dash", line_color="green")
 
 fig.write_html( 'output_file_name.html',
                    auto_open=True )
